404ab0eb6bf8e19b24b09f35fd821450b4e00a09
Snir David Nahari <snir-david@users.noreply.github.com>
2021-07-11 16:31:55 +0300

    thread pool with number threads

--- src/ThreadPool.java
+++ src/ThreadPool.java
@@ -1,30 +1,44 @@
-public class ThreadPool extends Thread {
-    Runnable task;
-    boolean term = false;
+import java.util.ArrayList;
+import java.util.List;
 
-    public ThreadPool() {
-        new Thread(() -> {
-            this.run();
-        }).start();
+public class ThreadPool extends Thread {
+    boolean term = false;
+    List<Thread> tp = new ArrayList<>();
+    List<Runnable> tasks = new ArrayList<>();
+
+
+    public ThreadPool(Integer num) {
+        for (int i = 0; i < num; i++)
+            tp.add(new Thread(() -> {
+                this.run();
+            }));
+        for (int i = 0; i < num; i++)
+            tp.get(i).start();
     }
 
-    public void assignTask(Runnable r) {
-        task = r;
+    public synchronized void assignTask(Runnable r) {
+        tasks.add(r);
         notifyPool();
     }
 
     @Override
     public void run() {
         while (!term) {
-            if (task != null)
-                task.run();
-            try {
-                suspendPool();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            synchronized (this) {
+                if (!tasks.isEmpty()) {
+                    tasks.get(0).run();
+                    tasks.remove(0);
+                    System.out.println("Thread " + Thread.currentThread().getId() + " got the task done!");
+                }
+                try {
+                    suspendPool();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
         System.out.println("Thread Pool is Dead!");
+        tp.remove(this);
     }
 
     synchronized void notifyPool() {
@@ -40,14 +54,19 @@ public class ThreadPool extends Thread {
     }
 
     public static void main(String[] args) {
-        ThreadPool tp = new ThreadPool();
-        tp.assignTask(() -> {
-            System.out.println("New Task added!");
-        });
-        tp.assignTask(() -> {
+        ThreadPool tp = new ThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            tp.assignTask(() -> {
+                System.out.println("New " + Integer.toString(finalI) + " Task added!");
+            });
+        }
+        tp.assignTask(()->{
             tp.terminatePool();
         });
-
+        while (!tp.tasks.isEmpty() && !tp.tp.isEmpty()){
+            tp.notifyPool();
+        }
 
     }
 }
