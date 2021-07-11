bcb7784a0989df202a07657903e7cb2f175f59f3
Snir David Nahari <snir-david@users.noreply.github.com>
2021-07-11 18:00:32 +0300

    GuardedSuspension dp

--- src/GuardedSuspension.java
+++ src/GuardedSuspension.java
@@ -1,2 +1,36 @@
 public class GuardedSuspension {
+    boolean someCondition = false;
+    int someDest = 0;
+
+    synchronized void doSomethingIfCond() throws InterruptedException {
+        while (!someCondition) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        System.out.println("Condition is met!");
+    }
+
+    synchronized void changeDest(int x) {
+        someDest += x;
+        if (someDest > 100) {
+            someCondition = true;
+            notifyAll();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        GuardedSuspension gs = new GuardedSuspension();
+        new Thread(() -> {
+            try {
+                gs.doSomethingIfCond();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        for (int i = 0; i < 15; i++) {
+            gs.changeDest(10);
+        }
+    }
 }
